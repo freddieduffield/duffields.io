1f66398b9599ef952cacdc3b10082a17a02cc921
freddieduffield <fwhduffield@gmail.com>
2024-07-12 15:21:46 +0100

    initial commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Holtwood_One_SC, DM_Sans } from 'next/font/google';
+
+export const holtwood = Holtwood_One_SC({
+    weight: '400',
+    subsets: ['latin']
+})
+
+export const dmSans = DM_Sans({
+    weight: '400',
+    subsets: ['latin']
+})
